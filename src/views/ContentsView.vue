9cebe8a32326362ac21b2ae87e3944c52e3f8bfb
Park Sohee <pssh5124@gmail.com>
2022-11-23 16:40:07 +0900

    feat : contents page end

--- src/views/ContentsView.vue
+++ src/views/ContentsView.vue
@@ -1,16 +1,16 @@
 <template>
-<SubTitle :title="title" :subTitle="subTitle" :text="text" :bg="bg" />
+  <SubTitle :title="title" :subTitle="subTitle" :text="text" :bg="bg" />
+  <ContentsList />
 </template>
 
 <script setup>
-import SubTitle from '../components/common/SubTitle.vue';
+import SubTitle from '../components/common/SubTitle.vue'
+import ContentsList from '../components/contents/ContentsList.vue'
 
 const title = `IDEACONCERT's Contents \n Introduction`
-const subTitle = "아이디어 콘서트에서 제작한 콘텐츠 리스트입니다."
+const subTitle = '아이디어 콘서트에서 제작한 콘텐츠 리스트입니다.'
 const text = `원작 스토리를 몰입도 높은 웹툰으로 제작하고 있습니다.\n 웹툰과 만화책을 영상화한 웹툰무비 작품들도 함께 만나 보세요.`
-const bg = "/assets/images/contents/Contents.jpg"
+const bg = '/assets/images/contents/Contents.jpg'
 </script>
-
-
 
 <style scoped></style>
