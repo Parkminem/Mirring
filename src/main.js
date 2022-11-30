c87d0d26c0c169f1068f9860f0c29b040a1f54d3
Park Sohee <pssh5124@gmail.com>
2022-11-30 10:04:48 +0900

    다국어 파일 add

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './routes/index';
+// import i18n from './i18n';
 
 const app = createApp(App);
 const pinia = createPinia();
