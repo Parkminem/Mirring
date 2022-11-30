951d7af94efe688ae683dd8c9368c7db1684e299
Park Sohee <pssh5124@gmail.com>
2022-11-30 15:58:40 +0900

    다국어 작업중

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,23 +1,13 @@
-// const requireLang = require.context('./locales', true, /\.json$/);
-// const messages = {};
+import { createI18n } from 'vue-i18n';
+import ko from './locales/ko.json';
+import pt from './locales/pt.json';
 
-// for (const file of requireLang.keys()) {
-//   if (file === './index.js') continue;
-//   const path = file.replace(/(\.\/|\.json$)/g).split('/');
-//   path.reduce((o, s, i) => {
-//     if (o[s]) return o[s];
-//     o[s] = i + 1 === path.length ? requireLang(file) : {};
-//     return o[s];
-//   }, messages);
-// }
-
-// Vue.use(VueI18n);
-
-// const i18n = new VueI18n({
-//   locale: 'ko',
-//   fallbackLocale: 'ko',
-//   messages,
-//   silentFallbackWarn: true
-// });
-
-// export default i18n;
+const i18n = createI18n({
+  locale: 'ko',
+  legacy: false,
+  allowComposition: true,
+  fallbackLocale: 'en',
+  silentFallbackWarn: true,
+  messages: { ko, pt }
+});
+export default i18n;
