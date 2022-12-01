0301e8ac14c5920c66c0a5c4502b366417028510
Minhyuk Park <comon23463@gmail.com>
2022-12-01 11:23:07 +0900

    language fix

--- src/components/tech/AutoCut.vue
+++ src/components/tech/AutoCut.vue
@@ -2,39 +2,33 @@
   <div class="catroon__cut bg__gray">
     <div class="container">
       <h2 class="content__title">{{ t('tech.separateTitle') }}</h2>
-      <p class="content__subtitle">
-        만화의 컷을 AI기술을 이용하여 자동으로 분리하고,<br />
-        텍스트와 장면을 인지하여 다양한 연출효과를 주는 영상 제작기술입니다.
-      </p>
+      <p class="content__subtitle" v-html="t('tech.separateText').replace('\n', '<br>')"></p>
 
       <div class="cut__images">
-        <div id="first__notice" class="content__notice">100%자동화 고도화 중</div>
+        <div id="first__notice" class="content__notice">{{ t('tech.separateBtn') }}</div>
         <div class="cut__image"></div>
         <ul class="cut__process">
           <li class="process process1 shadow">
             <div class="process__image"></div>
-            <div class="process__text">만화 스캔본 업로드</div>
+            <div class="process__text">{{ t('tech.separateBox01') }}</div>
             <div class="peocess__number">1</div>
           </li>
           <li class="process process2 shadow">
             <div class="process__image"></div>
-            <div class="process__text">컷 분석을 통한 흐름설정</div>
+            <div class="process__text">{{ t('tech.separateBox02') }}</div>
             <div class="peocess__number">2</div>
           </li>
           <li class="process process3 shadow">
             <div class="process__image"></div>
             <div class="process__text">
-              <span>자동 컷 인지 및 추출<br />회사소개 영상 제작 </span>
+              <span v-html="t('tech.separateBox03').replace('\n', '<br>')"></span>
             </div>
             <div class="peocess__number">3</div>
           </li>
           <li class="process process4 shadow">
             <div class="process__image"></div>
             <div class="process__text">
-              <span
-                >컷 분석을 통한 화면 연출, <br />
-                장면전환 매칭 / 매칭 시간 설정
-              </span>
+              <span v-html="t('tech.separateBox04').replace('\n', '<br>')"></span>
             </div>
             <div class="peocess__number">4</div>
           </li>
--- src/components/tech/Reditoon.vue
+++ src/components/tech/Reditoon.vue
@@ -2,17 +2,15 @@
   <div class="catroon__reditoon">
     <div class="container">
       <h2 class="content__title">{{ t('tech.autoToolTitle') }}</h2>
-      <p class="content__subtitle">
-        {{ t('tech.autoToolText') }}
-      </p>
+      <p class="content__subtitle" v-html="t('tech.autoToolText').replace('\n', '<br>')"></p>
 
       <div class="content__notice">
         <a
           style="color: white; font-family: NotoSans"
           href="/resources/common/images/tech/04_종이만화 자동 웹툰화 저작툴_카달로그(국문).pdf"
           target="_blank"
-          >온라인 소개서 보기</a
-        >
+          v-html="t('tech.autoToolBtn').replace('\n', '<br>')"
+        ></a>
       </div>
       <div class="reditoon__image_kr shadow"></div>
     </div>
