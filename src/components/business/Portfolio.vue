272fd8f00bd4f0c6059e16e980e49c7bd45e8ecd
Minhyuk Park <comon23463@gmail.com>
2022-12-05 11:11:43 +0900

    feat: swiper in business

--- src/components/business/Portfolio.vue
+++ src/components/business/Portfolio.vue
@@ -15,75 +15,50 @@
         <!-- slide -->
         <div class="slider_area">
           <div class="swiper-container swiper-container-initialized swiper-container-horizontal">
-            <div class="swiper-wrapper" style="transform: translate3d(-1104px, 0px, 0px); transition-duration: 0ms">
-              <div
-                class="swiper-slide swiper-slide-duplicate"
-                data-swiper-slide-index="16"
-                style="width: 248px; margin-right: 28px"
-              >
+            <Swiper
+              :loop="true"
+              :slides-per-view="2"
+              :space-between="30"
+              :allowTouchMove="true"
+              :breakpoints="breakPoints"
+              @swiper="onSwiper"
+            >
+              <SwiperSlide data-swiper-slide-index="0" class="css">
                 <a href="https://www.youtube.com/watch?v=EsE0GylCp68" target="_blank">
                   <img class="images" src="https://i.ytimg.com/vi/EsE0GylCp68/mqdefault.jpg" />
                 </a>
                 <div class="text_box">
                   <p class="title ellipsis">이 구역의 미친놈은 나야! 무림 최고 영재 미친놈 "천라신조"│웹툰무비</p>
                 </div>
-              </div>
-              <div
-                class="swiper-slide swiper-slide-duplicate"
-                data-swiper-slide-index="17"
-                style="width: 248px; margin-right: 28px"
-              >
+              </SwiperSlide>
+
+              <SwiperSlide data-swiper-slide-index="1" class="css">
                 <a href="https://www.youtube.com/watch?v=1_FC3mAchTE" target="_blank">
                   <img class="images" src="https://i.ytimg.com/vi/1_FC3mAchTE/mqdefault.jpg" />
                 </a>
                 <div class="text_box">
                   <p class="title ellipsis">동년배가 추천하는 클라스가 다른 강호의 제왕, 웹툰무비 "혈해전기"</p>
                 </div>
-              </div>
-              <div
-                class="swiper-slide swiper-slide-duplicate"
-                data-swiper-slide-index="18"
-                style="width: 248px; margin-right: 28px"
-              >
+              </SwiperSlide>
+
+              <SwiperSlide data-swiper-slide-index="2" class="css">
                 <a href="https://www.youtube.com/watch?v=4J-zAQnvWvE" target="_blank">
                   <img class="images" src="https://i.ytimg.com/vi/4J-zAQnvWvE/mqdefault.jpg" />
                 </a>
                 <div class="text_box">
                   <p class="title ellipsis">무술에 대한 모든 것? 나는 꼼수다, 아니 고수다! 웹툰무비 "하수전설"</p>
                 </div>
-              </div>
-              <div
-                class="swiper-slide swiper-slide-duplicate swiper-slide-prev"
-                data-swiper-slide-index="19"
-                style="width: 248px; margin-right: 28px"
-              >
+              </SwiperSlide>
+              <SwiperSlide data-swiper-slide-index="3" class="css">
                 <a href="https://www.youtube.com/watch?v=Xu3U3WThK5Q" target="_blank">
                   <img class="images" src="https://i.ytimg.com/vi/Xu3U3WThK5Q/mqdefault.jpg" />
                 </a>
                 <div class="text_box">
                   <p class="title ellipsis">브라이트하우스</p>
                 </div>
-              </div>
-              <!-- item -->
+              </SwiperSlide>
 
-              <div
-                class="swiper-slide swiper-slide-active"
-                data-swiper-slide-index="0"
-                style="width: 248px; margin-right: 28px"
-              >
-                <a href="https://www.youtube.com/watch?v=C5EdRd7-RAU" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/C5EdRd7-RAU/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">LA 여행 중 FBI 범죄에 연루되다?! "로맨틱 홀리데이"</p>
-                </div>
-              </div>
-
-              <div
-                class="swiper-slide swiper-slide-next"
-                data-swiper-slide-index="1"
-                style="width: 248px; margin-right: 28px"
-              >
+              <SwiperSlide data-swiper-slide-index="4" class="css">
                 <a href="https://www.youtube.com/watch?v=zwHZMBbs1nc" target="_blank">
                   <img class="images" src="https://i.ytimg.com/vi/zwHZMBbs1nc/mqdefault.jpg" />
                 </a>
@@ -93,38 +68,9 @@
                     마녀"
                   </p>
                 </div>
-              </div>
+              </SwiperSlide>
 
-              <div class="swiper-slide" data-swiper-slide-index="2" style="width: 248px; margin-right: 28px">
-                <a href="https://www.youtube.com/watch?v=30vO0r1BEWw" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/30vO0r1BEWw/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">
-                    웹툰무비│ 세상에 이런일이! 말하는 개, 히바가 있는 애견동반 게스트하우스 "브라이트 하우스"
-                  </p>
-                </div>
-              </div>
-
-              <div class="swiper-slide" data-swiper-slide-index="3" style="width: 248px; margin-right: 28px">
-                <a href="https://www.youtube.com/watch?v=59C10MYjzkY" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/59C10MYjzkY/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">붕가붕가, 마운팅 계속하는 이유가 궁금하면? 웹툰무비 "브라이트 하우스"</p>
-                </div>
-              </div>
-
-              <div class="swiper-slide" data-swiper-slide-index="4" style="width: 248px; margin-right: 28px">
-                <a href="https://www.youtube.com/watch?v=M40-OxhVOwM" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/M40-OxhVOwM/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">웹툰무비│재벌가 도련님의 은밀한 이중생활 "상수와 합정사이"</p>
-                </div>
-              </div>
-
-              <div class="swiper-slide" data-swiper-slide-index="5" style="width: 248px; margin-right: 28px">
+              <SwiperSlide data-swiper-slide-index="5" class="css">
                 <a href="https://www.youtube.com/watch?v=dcUoL-qb36s" target="_blank">
                   <img class="images" src="https://i.ytimg.com/vi/dcUoL-qb36s/mqdefault.jpg" />
                 </a>
@@ -133,203 +79,12 @@
                     웹툰무비│금융회사인줄 알고 취업했는데 조폭이 사채업하는 막장회사 "어둠의 자식들"
                   </p>
                 </div>
-              </div>
-
-              <div class="swiper-slide" data-swiper-slide-index="6" style="width: 248px; margin-right: 28px">
-                <a href="https://www.youtube.com/watch?v=2RLqrQX7JWo" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/2RLqrQX7JWo/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">웹툰무비│게임 경험치 현실에서 쓰는 방법 "워블"</p>
-                </div>
-              </div>
-
-              <div class="swiper-slide" data-swiper-slide-index="7" style="width: 248px; margin-right: 28px">
-                <a href="https://www.youtube.com/watch?v=oVX2E-MML4Q" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/oVX2E-MML4Q/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">웹툰무비│고민을 모두 작살내는 공포의 곰인형 "작살무개념"</p>
-                </div>
-              </div>
-
-              <div class="swiper-slide" data-swiper-slide-index="8" style="width: 248px; margin-right: 28px">
-                <a href="https://www.youtube.com/watch?v=DVbW_cfSiIk" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/DVbW_cfSiIk/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">
-                    웹툰무비│카드빚으로 일가족 파탄, 아버지는 극단적인 선택, 어머니 마저..."쩐의 전쟁"
-                  </p>
-                </div>
-              </div>
-
-              <div class="swiper-slide" data-swiper-slide-index="9" style="width: 248px; margin-right: 28px">
-                <a href="https://www.youtube.com/watch?v=a8Jb_BTL4O4" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/a8Jb_BTL4O4/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">웹툰무비│지우개를 먹고 사는 이상한 외계 생명체 "지우개똥"</p>
-                </div>
-              </div>
-
-              <div class="swiper-slide" data-swiper-slide-index="10" style="width: 248px; margin-right: 28px">
-                <a href="https://www.youtube.com/watch?v=xiy8hAt43mc" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/xiy8hAt43mc/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">웹툰무비│워싱턴 백악관에 여왕과 기사단이? "철수를 찾아서"</p>
-                </div>
-              </div>
-
-              <div class="swiper-slide" data-swiper-slide-index="11" style="width: 248px; margin-right: 28px">
-                <a href="https://www.youtube.com/watch?v=L7U7fH108BY" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/L7U7fH108BY/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">웹툰무비│광기 어린 괴물에 맞서기 위한 사투극 "두개의 달"</p>
-                </div>
-              </div>
-
-              <div class="swiper-slide" data-swiper-slide-index="12" style="width: 248px; margin-right: 28px">
-                <a href="https://www.youtube.com/watch?v=WMo4Ran8ZHk" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/WMo4Ran8ZHk/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">
-                    웹툰무비│식당에서 고기굽다 남자가 다른 남자랑 하룻밤 보내게 된 썰 "푸드메이트"
-                  </p>
-                </div>
-              </div>
-
-              <div class="swiper-slide" data-swiper-slide-index="13" style="width: 248px; margin-right: 28px">
-                <a href="https://www.youtube.com/watch?v=TIQ_YAkx380" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/TIQ_YAkx380/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">
-                    웹툰무비│외계인이라고 믿었던 친구를 대학교에서 다시 만난다면? "우주유실물"
-                  </p>
-                </div>
-              </div>
-
-              <div class="swiper-slide" data-swiper-slide-index="14" style="width: 248px; margin-right: 28px">
-                <a href="https://www.youtube.com/watch?v=YRplRLzI_EI" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/YRplRLzI_EI/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">물리치료사 손에 기적이 찾아왔다! 웹툰무비 "기적의 물리치료사"</p>
-                </div>
-              </div>
-
-              <div class="swiper-slide" data-swiper-slide-index="15" style="width: 248px; margin-right: 28px">
-                <a href="https://www.youtube.com/watch?v=jMAo3wT6ye0" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/jMAo3wT6ye0/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">만취 되서 망나니한테 조직이 무너지는 줄도 모르는 웹툰무비 "망나니"</p>
-                </div>
-              </div>
-
-              <div class="swiper-slide" data-swiper-slide-index="16" style="width: 248px; margin-right: 28px">
-                <a href="https://www.youtube.com/watch?v=EsE0GylCp68" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/EsE0GylCp68/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">이 구역의 미친놈은 나야! 무림 최고 영재 미친놈 "천라신조"│웹툰무비</p>
-                </div>
-              </div>
-
-              <div class="swiper-slide" data-swiper-slide-index="17" style="width: 248px; margin-right: 28px">
-                <a href="https://www.youtube.com/watch?v=1_FC3mAchTE" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/1_FC3mAchTE/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">동년배가 추천하는 클라스가 다른 강호의 제왕, 웹툰무비 "혈해전기"</p>
-                </div>
-              </div>
-
-              <div class="swiper-slide" data-swiper-slide-index="18" style="width: 248px; margin-right: 28px">
-                <a href="https://www.youtube.com/watch?v=4J-zAQnvWvE" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/4J-zAQnvWvE/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">무술에 대한 모든 것? 나는 꼼수다, 아니 고수다! 웹툰무비 "하수전설"</p>
-                </div>
-              </div>
-
-              <div
-                class="swiper-slide swiper-slide-duplicate-prev"
-                data-swiper-slide-index="19"
-                style="width: 248px; margin-right: 28px"
-              >
-                <a href="https://www.youtube.com/watch?v=Xu3U3WThK5Q" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/Xu3U3WThK5Q/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">브라이트하우스</p>
-                </div>
-              </div>
-
-              <!-- //item -->
-              <div
-                class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-active"
-                data-swiper-slide-index="0"
-                style="width: 248px; margin-right: 28px"
-              >
-                <a href="https://www.youtube.com/watch?v=C5EdRd7-RAU" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/C5EdRd7-RAU/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">LA 여행 중 FBI 범죄에 연루되다?! "로맨틱 홀리데이"</p>
-                </div>
-              </div>
-              <div
-                class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-next"
-                data-swiper-slide-index="1"
-                style="width: 248px; margin-right: 28px"
-              >
-                <a href="https://www.youtube.com/watch?v=zwHZMBbs1nc" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/zwHZMBbs1nc/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">
-                    웹툰무비│역대급 원조 아이돌스타와 천만 여배우 사이에서 태어난 악마같은 재능을 가진 소녀 "녹스의
-                    마녀"
-                  </p>
-                </div>
-              </div>
-              <div
-                class="swiper-slide swiper-slide-duplicate"
-                data-swiper-slide-index="2"
-                style="width: 248px; margin-right: 28px"
-              >
-                <a href="https://www.youtube.com/watch?v=30vO0r1BEWw" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/30vO0r1BEWw/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">
-                    웹툰무비│ 세상에 이런일이! 말하는 개, 히바가 있는 애견동반 게스트하우스 "브라이트 하우스"
-                  </p>
-                </div>
-              </div>
-              <div
-                class="swiper-slide swiper-slide-duplicate"
-                data-swiper-slide-index="3"
-                style="width: 248px; margin-right: 28px"
-              >
-                <a href="https://www.youtube.com/watch?v=59C10MYjzkY" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/59C10MYjzkY/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">붕가붕가, 마운팅 계속하는 이유가 궁금하면? 웹툰무비 "브라이트 하우스"</p>
-                </div>
-              </div>
-            </div>
-            <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>
+              </SwiperSlide>
+            </Swiper>
           </div>
-          <!-- Add Arrows -->
+          <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>
           <img
+            @click="swiper.slidePrev()"
             class="swiper_button_prev swiper_navigation"
             src="/assets/images/business/swiper_prev.png"
             alt="prev"
@@ -339,6 +94,7 @@
             aria-label="Previous slide"
           />
           <img
+            @click="swiper.slideNext()"
             class="swiper_button_next swiper_navigation"
             src="/assets/images/business/swiper_next.png"
             alt="next"
@@ -347,16 +103,32 @@
             role="button"
             aria-label="Next slide"
           />
+
+          <!-- slide -->
+          <a class="page_move" href="https://www.youtube.com/channel/UChWHJjTGDc4314owWJ7KGCw" target="_blank"
+            >{{ t('common.button.youtubeLink') }} &gt;</a
+          >
         </div>
-        <!-- slide -->
-        <a class="page_move" href="https://www.youtube.com/channel/UChWHJjTGDc4314owWJ7KGCw" target="_blank"
-          >{{ t('common.button.youtubeLink') }} &gt;</a
-        >
       </div>
     </div>
   </section>
 </template>
 <script setup>
 import { useI18n } from 'vue-i18n';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import { ref } from 'vue';
+
 const { t } = useI18n();
+const swiper = ref(null);
+let onSwiper = (swiperInstance) => {
+  swiper.value = swiperInstance;
+};
+const breakPoints = {
+  1024: {
+    spaceBetween: 28,
+    slidesPerView: 4
+  }
+};
 </script>
+<style></style>
