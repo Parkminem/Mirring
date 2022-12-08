3fa98c345de485dadce98e7f490f5982ab09f6aa
Park Sohee <pssh5124@gmail.com>
2022-12-08 15:47:50 +0900

    feat : 네비게이션 스크롤 이벤트 코드 수정

--- src/components/home/NavigationBar.vue
+++ src/components/home/NavigationBar.vue
@@ -11,7 +11,7 @@
   </nav>
 </template>
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, onUnmounted } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
 const router = useRouter();
 const route = useRoute();
@@ -33,31 +33,30 @@ let dots = ref({
   5: false
 });
 
+//네비게이션 스크롤 이벤트
+function scrollActive() {
+  let scrollTop = scrollY;
+  if (Math.round(document.querySelector('.dot1').offsetTop - 100) < scrollTop) {
+    [dots.value[1], dots.value[2], dots.value[3], dots.value[4], dots.value[5]] = [true, false, false, false, false];
+  }
+  if (Math.round(document.querySelector('.dot2').offsetTop - 100) < scrollTop) {
+    [dots.value[1], dots.value[2], dots.value[3], dots.value[4], dots.value[5]] = [false, true, false, false, false];
+  }
+  if (Math.round(document.querySelector('.dot3').offsetTop - 400) < scrollTop) {
+    [dots.value[1], dots.value[2], dots.value[3], dots.value[4], dots.value[5]] = [false, false, true, false, false];
+  }
+  if (Math.round(document.querySelector('.dot4').offsetTop - 100) < scrollTop) {
+    [dots.value[1], dots.value[2], dots.value[3], dots.value[4], dots.value[5]] = [false, false, false, true, false];
+  }
+  if (Math.round(document.querySelector('.dot5').offsetTop - 100) < scrollTop) {
+    [dots.value[1], dots.value[2], dots.value[3], dots.value[4], dots.value[5]] = [false, false, false, false, true];
+  }
+}
 onMounted(() => {
-  //네비게이션 스크롤 이벤트
-  const section01H = Math.floor(document.querySelector('.dot1').getBoundingClientRect().y - 100);
-  const section02H = Math.floor(document.querySelector('.dot2').getBoundingClientRect().y - 100);
-  const section03H = Math.floor(document.querySelector('.dot3').getBoundingClientRect().y - 400);
-  const section04H = Math.floor(document.querySelector('.dot4').getBoundingClientRect().y - 100);
-  const section05H = Math.floor(document.querySelector('.dot5').getBoundingClientRect().y - 100);
-  window.addEventListener('scroll', () => {
-    let scrollTop = scrollY;
-    if (section01H < scrollTop) {
-      [dots.value[1], dots.value[2], dots.value[3], dots.value[4], dots.value[5]] = [true, false, false, false, false];
-    }
-    if (section02H < scrollTop) {
-      [dots.value[1], dots.value[2], dots.value[3], dots.value[4], dots.value[5]] = [false, true, false, false, false];
-    }
-    if (section03H < scrollTop) {
-      [dots.value[1], dots.value[2], dots.value[3], dots.value[4], dots.value[5]] = [false, false, true, false, false];
-    }
-    if (section04H < scrollTop) {
-      [dots.value[1], dots.value[2], dots.value[3], dots.value[4], dots.value[5]] = [false, false, false, true, false];
-    }
-    if (section05H < scrollTop) {
-      [dots.value[1], dots.value[2], dots.value[3], dots.value[4], dots.value[5]] = [false, false, false, false, true];
-    }
-  });
+  window.addEventListener('scroll', scrollActive);
+});
+onUnmounted(() => {
+  window.removeEventListener('scroll', scrollActive);
 });
 </script>
 <style scoped>
