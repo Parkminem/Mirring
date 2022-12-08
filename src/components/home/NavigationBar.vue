bf920ef62f2f1f86f8a0a7c3b3fd98268fa5f030
Park Sohee <pssh5124@gmail.com>
2022-12-08 17:02:32 +0900

    네비게이션 이벤트 스크롤 높이 수정

--- src/components/home/NavigationBar.vue
+++ src/components/home/NavigationBar.vue
@@ -8,8 +8,8 @@ import { onMounted, ref, onUnmounted } from 'vue';
 
 //navigation 클릭 시 섹션 이동 함수
 function goSection(num) {
-  window.scrollTo({ top: document.querySelector(`.dot${num}`).offsetTop - 100, behavior: 'smooth' });
-  if (num == 3) window.scrollTo({ top: document.querySelector(`.dot${num}`).offsetTop - 400, behavior: 'smooth' });
+  window.scrollTo({ top: document.querySelector(`.dot${num}`).offsetTop, behavior: 'smooth' });
+  if (num == 3) window.scrollTo({ top: document.querySelector(`.dot${num}`).offsetTop - 300, behavior: 'smooth' });
 }
 
 let dots = ref({
@@ -23,19 +23,19 @@ let dots = ref({
 //네비게이션 스크롤 이벤트
 function scrollActive() {
   let scrollTop = scrollY;
-  if (Math.round(document.querySelector('.dot1').offsetTop - 100) <= scrollTop) {
+  if (Math.round(document.querySelector('.dot1').offsetTop) <= scrollTop) {
     [dots.value[1], dots.value[2], dots.value[3], dots.value[4], dots.value[5]] = [true, false, false, false, false];
   }
-  if (Math.round(document.querySelector('.dot2').offsetTop - 100) <= scrollTop) {
+  if (Math.round(document.querySelector('.dot2').offsetTop) <= scrollTop) {
     [dots.value[1], dots.value[2], dots.value[3], dots.value[4], dots.value[5]] = [false, true, false, false, false];
   }
-  if (Math.round(document.querySelector('.dot3').offsetTop - 400) <= scrollTop) {
+  if (Math.round(document.querySelector('.dot3').offsetTop - 300) <= scrollTop) {
     [dots.value[1], dots.value[2], dots.value[3], dots.value[4], dots.value[5]] = [false, false, true, false, false];
   }
-  if (Math.round(document.querySelector('.dot4').offsetTop - 100) <= scrollTop) {
+  if (Math.round(document.querySelector('.dot4').offsetTop) <= scrollTop) {
     [dots.value[1], dots.value[2], dots.value[3], dots.value[4], dots.value[5]] = [false, false, false, true, false];
   }
-  if (Math.round(document.querySelector('.dot5').offsetTop - 100) <= scrollTop) {
+  if (Math.round(document.querySelector('.dot5').offsetTop) <= scrollTop) {
     [dots.value[1], dots.value[2], dots.value[3], dots.value[4], dots.value[5]] = [false, false, false, false, true];
   }
 }
