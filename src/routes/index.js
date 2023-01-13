e5be9c78e6672477279abda04b20b796566ac9d1
taezzang <eaqwrd@gmail.com>
2023-01-13 14:57:08 +0900

    라우터 Dynamic Import 코드 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,53 +1,58 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Layout from '../layout/Layout.vue';
-import MainView from '../views/MainView.vue';
-import AboutUsView from '../views/AboutUsView.vue';
-import BusinessView from '../views/BusinessView.vue';
-import CareersView from '../views/CareersView.vue';
-import ContactView from '../views/ContactView.vue';
-import ContentsView from '../views/ContentsView.vue';
-import OurTechView from '../views/OurTechView.vue';
-import DetailNewsView from '../views/DetailNewsView.vue';
+
+// 이렇게 View 컴포넌트들을 미리 임포트 시키는 건 최초 로딩 시간에 영향을 줍니다.
+// Dynamic Import (동적 코드 분할, Lazy Loading)를 통해 동적으로 임포트합시다.
+// 사용법은 import() 함수를 이용하는 것입니다.
+// ex) component: () => import('../views/MainView.vue')
+
+// import Layout from '../layout/Layout.vue';
+// import MainView from '../views/MainView.vue';
+// import AboutUsView from '../views/AboutUsView.vue';
+// import BusinessView from '../views/BusinessView.vue';
+// import CareersView from '../views/CareersView.vue';
+// import ContactView from '../views/ContactView.vue';
+// import ContentsView from '../views/ContentsView.vue';
+// import OurTechView from '../views/OurTechView.vue';
+// import DetailNewsView from '../views/DetailNewsView.vue';
 
 const routes = [
   {
     path: '/',
     name: 'layout',
-    component: Layout,
+    component: () => import('@/layout/Layout.vue'),
     children: [
       {
         path: '',
         name: 'Home',
-        component: MainView
+        component: () => import('@/views/MainView.vue')
       },
       {
         path: '/about',
-        name: 'about',
-        component: AboutUsView
+        component: () => import('@/views/AboutUsView.vue')
       },
       {
         path: '/business',
-        component: BusinessView
+        component: () => import('@/views/BusinessView.vue')
       },
       {
         path: '/careers',
-        component: CareersView
+        component: () => import('@/views/CareersView.vue')
       },
       {
         path: '/contact',
-        component: ContactView
+        component: () => import('@/views/ContactView.vue')
       },
       {
         path: '/contents',
-        component: ContentsView
+        component: () => import('@/views/ContentsView.vue')
       },
       {
         path: '/tech',
-        component: OurTechView
+        component: () => import('@/views/OurTechView.vue')
       },
       {
         path: '/detail',
-        component: DetailNewsView,
+        component: () => import('@/views/DetailNewsView.vue'),
         name: 'detail'
       }
     ]
@@ -55,7 +60,7 @@ const routes = [
   {
     path: '/notfound',
     name: '404',
-    component: () => import('../layout/ErrorLayout.vue')
+    component: () => import('@/layout/ErrorLayout.vue')
   },
   { path: '/:pathMatch(.*)*', redirect: '/notfound' }
 ];
