d321560bf34d374d7cd65002aa21a0b85a2d8031
Park Sohee <pssh5124@gmail.com>
2023-01-26 11:42:03 +0900

    feat : 뉴스 블로그 구현 완료(다음글버튼O)

--- src/components/detail/DetailBody.vue
+++ src/components/detail/DetailBody.vue
@@ -2,17 +2,23 @@
   <section class="section" data-name="about_detail_news">
     <div class="container_1076">
       <div class="notice_head">
-        <div class="title">{{ title }}</div>
-        <div class="date">{{ changeDate(date) }}</div>
+        <div class="title" v-if="locale === 'kr'">{{ detailNews.title_kr }}</div>
+        <div class="title" v-if="locale === 'en'">{{ detailNews.title_us }}</div>
+        <div class="title" v-if="locale === 'pt'">{{ detailNews.title_pt }}</div>
+        <div class="title" v-if="locale === 'id'">{{ detailNews.title_id }}</div>
+        <div class="date">{{ changeDate(detailNews.regdate) }}</div>
       </div>
-      <div class="notice_body" v-html="content"></div>
+      <div class="notice_body" v-html="detailNews.content_kr" v-if="locale === 'kr'"></div>
+      <div class="notice_body" v-html="detailNews.content_us" v-if="locale === 'en'"></div>
+      <div class="notice_body" v-html="detailNews.content_pt" v-if="locale === 'pt'"></div>
+      <div class="notice_body" v-html="detailNews.content_id" v-if="locale === 'id'"></div>
     </div>
     <ul class="notice_foot">
-      <li class="prev" v-if="preBtn()" @click="goPage(Number(pk) - 1)">&lt; {{ t('detail.prev') }}</li>
+      <li class="prev" v-if="detailNews.prev_pk" @click="goPage(detailNews.prev_pk)">&lt; {{ t('detail.prev') }}</li>
       <li class="back" @click="goNews">
         <a href="/about#about_news">{{ t('detail.list') }}</a>
       </li>
-      <li class="next" @click="goPage(Number(pk) + 1)">{{ t('detail.next') }} &gt;</li>
+      <li class="next" v-if="detailNews.next_pk" @click="goPage(detailNews.next_pk)">{{ t('detail.next') }} &gt;</li>
     </ul>
   </section>
 </template>
@@ -29,33 +35,13 @@ const props = defineProps({
   pk: String
 });
 
-const title = ref('');
-const content = ref('');
-const date = ref('');
+const detailNews = ref();
 
 //상세 뉴스 불러오기
 await aboutApi
   .getDetailNews(locale.value, props.pk)
   .then((res) => {
-    date.value = res.data.data.regdate;
-    switch (locale.value) {
-      case 'kr':
-        title.value = res.data.data.title_kr;
-        content.value = res.data.data.content_kr;
-        break;
-      case 'id':
-        title.value = res.data.data.title_id;
-        content.value = res.data.data.content_id;
-        break;
-      case 'en':
-        title.value = res.data.data.title_us;
-        content.value = res.data.data.content_us;
-        break;
-      case 'pt':
-        title.value = res.data.data.title_pt;
-        content.value = res.data.data.content_pt;
-        break;
-    }
+    detailNews.value = res.data.data;
   })
   .catch((err) => console.log(err));
 
@@ -70,13 +56,6 @@ function goNews() {
 const goPage = (num) => {
   window.location.href = `/detail?pk=${num}`;
 };
-
-//이전버튼 생성
-const preBtn = () => {
-  if (props.pk == '1') return false;
-  return true;
-};
-// 다음버튼 생성
 </script>
 <style lang="scss" scoped>
 .section {
