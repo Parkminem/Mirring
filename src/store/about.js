1b47e258dbcb5aae2d494113d8b50eddc494a357
Park Sohee <pssh5124@gmail.com>
2023-01-26 14:26:45 +0900

    feat : 뉴스블로그, 문의신청 완료

--- src/store/about.js
+++ src/store/about.js
@@ -52,7 +52,6 @@ export const useAboutStore = defineStore('about', {
       await aboutApi
         .getNews(locale, page)
         .then((res) => {
-          console.log(res);
           this.newsList = res.data.data.newsData;
           this.totalPage = res.data.data.totalPageNum;
         })
