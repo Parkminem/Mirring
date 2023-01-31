ad536db244b19b686ba5c2d199a066ce95a37c9f
Park Sohee <pssh5124@gmail.com>
2023-01-31 10:07:11 +0900

    feat : 포트폴리오 리스트 api 추가

--- src/components/business/Portfolio.vue
+++ src/components/business/Portfolio.vue
@@ -20,64 +20,21 @@
               :slides-per-view="2"
               :space-between="30"
               :allowTouchMove="true"
+              :looped-slides="portfolioList.length"
               :breakpoints="breakPoints"
               @swiper="onSwiper"
             >
-              <SwiperSlide data-swiper-slide-index="0" class="css">
-                <a href="https://www.youtube.com/watch?v=EsE0GylCp68" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/EsE0GylCp68/mqdefault.jpg" />
+              <SwiperSlide
+                :data-swiper-slide-index="idx"
+                class="css"
+                v-for="(item, idx) in portfolioList"
+                :key="item.id"
+              >
+                <a :href="`https://www.youtube.com/watch?v=${item.video_id}`" target="_blank">
+                  <img class="images" :src="item.thumbnail" />
                 </a>
                 <div class="text_box">
-                  <p class="title ellipsis">이 구역의 미친놈은 나야! 무림 최고 영재 미친놈 "천라신조"│웹툰무비</p>
-                </div>
-              </SwiperSlide>
-
-              <SwiperSlide data-swiper-slide-index="1" class="css">
-                <a href="https://www.youtube.com/watch?v=1_FC3mAchTE" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/1_FC3mAchTE/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">동년배가 추천하는 클라스가 다른 강호의 제왕, 웹툰무비 "혈해전기"</p>
-                </div>
-              </SwiperSlide>
-
-              <SwiperSlide data-swiper-slide-index="2" class="css">
-                <a href="https://www.youtube.com/watch?v=4J-zAQnvWvE" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/4J-zAQnvWvE/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">무술에 대한 모든 것? 나는 꼼수다, 아니 고수다! 웹툰무비 "하수전설"</p>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide data-swiper-slide-index="3" class="css">
-                <a href="https://www.youtube.com/watch?v=Xu3U3WThK5Q" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/Xu3U3WThK5Q/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">브라이트하우스</p>
-                </div>
-              </SwiperSlide>
-
-              <SwiperSlide data-swiper-slide-index="4" class="css">
-                <a href="https://www.youtube.com/watch?v=zwHZMBbs1nc" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/zwHZMBbs1nc/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">
-                    웹툰무비│역대급 원조 아이돌스타와 천만 여배우 사이에서 태어난 악마같은 재능을 가진 소녀 "녹스의
-                    마녀"
-                  </p>
-                </div>
-              </SwiperSlide>
-
-              <SwiperSlide data-swiper-slide-index="5" class="css">
-                <a href="https://www.youtube.com/watch?v=dcUoL-qb36s" target="_blank">
-                  <img class="images" src="https://i.ytimg.com/vi/dcUoL-qb36s/mqdefault.jpg" />
-                </a>
-                <div class="text_box">
-                  <p class="title ellipsis">
-                    웹툰무비│금융회사인줄 알고 취업했는데 조폭이 사채업하는 막장회사 "어둠의 자식들"
-                  </p>
+                  <p class="title ellipsis">{{ item.title }}</p>
                 </div>
               </SwiperSlide>
             </Swiper>
@@ -118,9 +75,12 @@ import { useI18n } from 'vue-i18n';
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import 'swiper/css';
 import { ref } from 'vue';
+import businessApi from '../../api/business';
 
 const { t } = useI18n();
 const swiper = ref(null);
+const portfolioList = ref();
+
 let onSwiper = (swiperInstance) => {
   swiper.value = swiperInstance;
 };
@@ -130,6 +90,15 @@ const breakPoints = {
     slidesPerView: 4
   }
 };
+//포트폴리오 리스트 조회
+businessApi
+  .getPortfolio()
+  .then((res) => {
+    portfolioList.value = res.data.data;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 </script>
 <style scoped lang="scss">
 .section {
