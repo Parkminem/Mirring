02da643d678b8c04f975e30b3c3a566dee787845
Park Sohee <pssh5124@gmail.com>
2023-02-07 09:15:43 +0900

    baseURL 변경

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL: import.meta.env.VITE_APP_BASE_URL
-  baseURL: 'http://222.239.218.187:9100'
+  baseURL: 'http://222.239.218.187:9100/common'
 });
 
 export default instance;
