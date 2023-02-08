fa4aa2dcfe28596d4f6676753a722d0e03438e8c
Park Sohee <pssh5124@gmail.com>
2023-02-08 09:35:08 +0900

    url 에 . 추가

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   // baseURL: import.meta.env.VITE_APP_BASE_URL
   // baseURL: 'https://222.239.218.187:9100/common'
-  baseURL: 'https://ideaconcertcom/common'
+  baseURL: 'https://ideaconcert.com/common'
 });
 
 export default instance;
